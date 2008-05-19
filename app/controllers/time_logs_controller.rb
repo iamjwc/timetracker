e8e6bdb5490b384e1f4855bbdce0e604c621c867
Justin Camerer <iamjwc@gmail.com>
2008-05-19 12:04:55 -0500

    TimeClock description works

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -4,6 +4,15 @@ class TimeLogsController < ApplicationController
     @time_logs = @customer.time_logs
   end
 
+  def update
+    @customer = Customer.find params[:customer_id]
+    @time_log = TimeLog.find params[:id]
+
+    @time_log.update_attributes(params[:time_log])
+
+    redirect_to customer_url(@customer)
+  end
+
   def start
     @customer = Customer.find params[:customer_id]
     @customer.start_working
